b9ee424f3eb862b02780ee05a9db21f9feeb579b
Ben Unsworth <ben.unsworth@pinpointdesigns.co.uk>
2020-07-15 23:46:29 +0100

    Removed debugging code from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,6 @@ fi
 
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
-ls -l
-
 echo "Installing composer..."
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -34,7 +32,6 @@ php -d memory_limit=-1 composer.phar global require hirak/prestissimo
 php -d memory_limit=-1 composer.phar install
 
 echo "Installing linters..."
-ls -l vendor/bitexpert/phpstan-magento
 # php -d memory_limit=-1 composer.phar require --dev phpstan/phpstan
 # php -d memory_limit=-1 composer.phar require --dev bitexpert/phpstan-magento
 
